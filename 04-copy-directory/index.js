55f02eae466afb50cd9567233c6061eb32a1119d
Taraikovich <71258577+Taraikovich@users.noreply.github.com>
2023-05-09 21:23:39 +0400

    fix: task 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -9,23 +9,36 @@ fs.access(folderCopy, (err) => {
     fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
       if (err) throw err;
     });
-  }
+    copyFiles();
+  } else {
+    fs.rm(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+        if (err) throw err;
+      });
+      copyFiles();
+    });
+  };
 });
 
-fs.readdir(folder, { withFileTypes: true }, (err, files) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  files.forEach((file) => {
-    if (file.isFile()) {
-      const copyFile = path.join(folderCopy, file.name);
-      const readFile = path.join(folder, file.name);
-      const input = fs.createReadStream(readFile, 'utf-8');
-      const output = fs.createWriteStream(copyFile);
-      input.pipe(output);
+function copyFiles() {
+  fs.readdir(folder, { withFileTypes: true }, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
     }
-  });
-});
 
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const copyFile = path.join(folderCopy, file.name);
+        const readFile = path.join(folder, file.name);
+        const input = fs.createReadStream(readFile, 'utf-8');
+        const output = fs.createWriteStream(copyFile);
+        input.pipe(output);
+      }
+    });
+  });
+}
